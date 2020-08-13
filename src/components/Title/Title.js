f01fdd63fc09aabcae4fe6965bf8bc14278ea5d6
Kyle Davis <kyled871@yahoo.com>
2020-08-13 18:35:32 -0400

    initial navbar

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Title.css";
 
-function Title(props) {
-  return <h1 className="title">{props.children}</h1>;
+function Title() {
+    return (
+        <nav className="navbar title">
+                <a href="/"><h1 className="brand">Mario Clicky Game</h1></a>
+                <h1 className="brand">Click an image to begin!</h1>
+                <h1 className="brand">Score: </h1>
+        </nav>
+
+    );
 }
 
 export default Title;
