472c62cb1414243a9e4ae5295691b644432e8af0
Kyle Davis <kyled871@yahoo.com>
2020-08-16 10:36:06 -0400

    heroku server setup

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"))
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("public"));
-}
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
