a6a4bdb879bee638e9a2270eeed598df18b65740
Kyle Davis <kyled871@yahoo.com>
2020-08-17 21:57:34 -0400

    add client side

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-const express = require("express")
-const path = require("path")
-const PORT = process.env.PORT || 8080
-const app = express()
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"))
-})
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`)
-})
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./public/index.html'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
+
+app.listen(PORT, function () {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+})
